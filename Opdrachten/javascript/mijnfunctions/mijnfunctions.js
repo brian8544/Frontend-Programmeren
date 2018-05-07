260d8a898035fdffd15af3fb9c8008d02a2d365d
Shadow <brianleon77@gmail.com>
2018-05-07 09:31:42 +0200

    Extra -- blz 182

--- Opdrachten/javascript/mijnfunctions/mijnfunctions.js
+++ Opdrachten/javascript/mijnfunctions/mijnfunctions.js
@@ -4,3 +4,19 @@ function afmelden() {
     alert('U wordt afgemeld!!!');
   }
 }
+
+function dollarKoers() {
+	return(1.36);
+}
+function euroKoers() {
+	return(0.74);
+}
+
+function wissel(bedrag, valuta) {
+	if(valuta == 'euro') {
+		return(bedrag * dollarKoers());
+	}
+	else if(valuta == 'dollar') {
+		return(bedrag * euroKoers());
+	}
+}
